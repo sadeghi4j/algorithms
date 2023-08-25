73cc9702c055ebdf6de995d9f543bcb0c90ad075
sadeghi4j <sadeghi4j@gmail.com>
2023-08-25 23:10:56 +0330

    - Solved _28FirstOccurrenceInString.java

--- src/main/java/algorithm/easy/_28FirstOccurrenceInString.java
+++ src/main/java/algorithm/easy/_28FirstOccurrenceInString.java
@@ -9,23 +9,25 @@ package algorithm.easy;
 public class _28FirstOccurrenceInString {
 
     public int strStr(String haystack, String needle) {
-        int j = 0;
-        for (int i = 0; i < haystack.length(); i++) {
-            if (j == needle.length()) {
-                return i - j;
-            }
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                j++;
+        int n = 0;
+        int h = 0;
+        while (h < haystack.length() && n < needle.length()) {
+            if (haystack.charAt(h) == needle.charAt(n)) {
+                n++;
             } else {
-                j = 0;
+                h = h - n;
+                n = 0;
             }
+            h++;
         }
-        return j;
+        return n == needle.length() ? h - n : -1;
     }
 
     public static void main(String[] args) {
 //        String haystack = "sadbutsad", needle = "sad";
-        String haystack = "leetcode", needle = "leeto";
+//        String haystack = "leetcode", needle = "leeto";
+//        String haystack = "cocode", needle = "code";
+        String haystack = "mississippi", needle = "issip";
         int str = new _28FirstOccurrenceInString().strStr(haystack, needle);
         System.out.println("str = " + str);
     }
